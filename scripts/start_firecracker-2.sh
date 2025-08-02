28ecee42c01b9f0a78783c0a47a1a947271ed786
afeefuddin <afeefud2004din@gmail.com>
2025-08-03 01:03:05 +0530

    let's test

--- scripts/start_firecracker-2.sh
+++ scripts/start_firecracker-2.sh
@@ -3,8 +3,7 @@ TAP_IP=$3
 MASK_SHORT="/30"
 
 # Setup network interface
-sudo ip link del "$TAP_DEV" 2> /dev/null || true
-sudo ip tuntap add dev "$TAP_DEV" mode tap
+sudo ip link del "$TAP_DEV" 2> /dev/null || true sudo ip tuntap add dev "$TAP_DEV" mode tap
 sudo ip addr add "${TAP_IP}${MASK_SHORT}" dev "$TAP_DEV"
 sudo ip link set dev "$TAP_DEV" up
 
